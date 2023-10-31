81bc28c777a9f2f086fd00b0c9294bca9fd76d95
Samuel Souza <smksouza.dev@gmail.com>
2023-10-31 17:10:34 -0300

    add require render as required code

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { server } from './app'
 server
   .listen({
     port: env.PORT,
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
     console.log('HTTP Server Running')
